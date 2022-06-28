bb74a41111e88ad91faace5d38d99189ef373a42
Olumide, S.A <simiolumide@gmail.com>
2022-06-28 10:23:33 -0700

    combination program that prints unique two digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+
+# this program all possible different
+# combination of two digits. e.g 01 and 10
+# are same. The two digits must be different
+
 for n in range(0, 10):
-   for m in range(0, 10):
-      for 
+    for m in range(n + 1, 10):
+        if n == 8 and m == 9:
+            print("{}{}".format(n, m))
+        else:
+            print("{}{}".format(n, m), end=", ")

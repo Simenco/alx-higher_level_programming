dd0696542b36419c5fcd4ab2add51ff949c2a71e
Olumide, S.A <simiolumide@gmail.com>
2022-06-28 12:14:39 -0700

    prints the last digit of a number

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -5,5 +5,7 @@
 def print_last_digit(number):
     digit = abs(number) % 10
     if (number < 0):
+        print("{}".format(-digit), end="")
         return -digit
+    print("{}".format(digit), end="")
     return digit

fdfb30d238d757556cf3a3f7f4aff384b6b0e936
Olumide, S.A <simiolumide@gmail.com>
2022-06-28 11:45:34 -0700

    changed the organization

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -6,8 +6,6 @@ def uppercase(str):
     for c in str:
         char = ord(c)
         if (char in range(97, 123)):
-            up = chr(char - 32)
-        else:
-            up = chr(char)
-        print("{}".format(up), end="")
-    print(end=" \n")
+            c = chr(char - 32)
+        print("{}".format(c), end="")
+    print("")

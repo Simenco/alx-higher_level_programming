f45db40003073e178c62e4ee7134274401143bc6
Olumide, S.A <simiolumide@gmail.com>
2022-06-30 15:55:26 -0700

    this program add two numbers using a module

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 
 """
-This program imports a module that adds two 
+This program imports a module that adds two
 integer numbers together and produces result
 
 """
-from add_0 import add
+if __name__ == "__main__":
+    from add_0 import add
 
-a = 1
-b = 2
-
-result = add(a, b)
-print("<a {}> + <b {}> = <add(a, b) {}>".format(a, b, result))
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))

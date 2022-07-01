5325b7f86c58f4b52fd234f60ac0e041c253e16b
Olumide, S.A <simiolumide@gmail.com>
2022-06-30 23:07:45 -0700

    simple calculator to take input from command line

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+"""
+This program imports all functions from the file calculator_1.py
+and handles basic operations
+
+"""
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import *
+
+    n = len(sys.argv)
+
+    if n != 4:
+        print("Usage: ./100-my_calculator.py a operator b\n")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+    operators = ["+", "-", "*", "/"]
+        
+    if operator not in operators:
+        print("unknown operator, Available operators: +, -, * and /\n")
+        sys.exit(1)
+
+    if operator == "+":
+        res = add(a, b)
+
+    elif operator == "-":
+        res = sub(a, b)
+
+    elif operator == "*":
+        res = mul(a, b)
+
+    else:
+        res = div(a, b)
+    print("{} {} {} = {}".format(a, operator, b, res))

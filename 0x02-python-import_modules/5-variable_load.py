7d60c4262a8afd9e91d4a42eff013f77304ed0b6
Olumide, S.A <simiolumide@gmail.com>
2022-06-30 21:57:09 -0700

    gave permission

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+this program prints the variable a from a file 
+and prints its value
+
+"""
+
+if __name__ == "__main__":
+
+    from variable_load_5 import a
+
+    print(a)

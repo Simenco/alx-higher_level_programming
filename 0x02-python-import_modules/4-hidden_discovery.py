64a1b5459f5d98e25c670f98e91e4268bbb3d23b
Olumide, S.A <simiolumide@gmail.com>
2022-06-30 21:51:37 -0700

    prints names in a hidden file

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+this program prints all the names defined by
+module hidden_4.pyc
+
+"""
+if __name__ == "__main__":
+    
+    
+    import hidden_4
+
+    names = dir(hidden_4)
+    for i in names:
+        if i[:2] != "__":
+            print(name)

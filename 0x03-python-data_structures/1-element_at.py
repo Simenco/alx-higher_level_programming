6e708c19030fda8370501394fce6cb4fe1e07046
Olumide, S.A <simiolumide@gmail.com>
2022-07-02 15:59:52 -0700

    retrieves an element from a list

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+This function retrieves an element from a list,
+"""
+
+
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+    ele = len(my_list) - 1
+
+    if idx > ele:
+        return None
+
+    for a in range(ele):
+        if idx == a:
+            return (my_list[idx])
+

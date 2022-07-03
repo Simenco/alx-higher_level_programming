c28669c8bd4d5296a9ea991254466b906055cfd7
Olumide, S.A <simiolumide@gmail.com>
2022-07-03 14:17:46 -0700

    prints elements of a list in reverse

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+this function prints alli integers of a list, 
+in reverse order, one integer per line
+"""
+
+
+def print_reversed_list_integer(my_list=[]):
+    for i in range(len(my_list)):
+        print("{}".format(my_list.pop()))

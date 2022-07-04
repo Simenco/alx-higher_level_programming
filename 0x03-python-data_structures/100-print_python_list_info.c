6b9fe6ff8e88117a1cea4c7e6c56e3dbcef6fbe9
Olumide, S. A <simiolumide@gmail.com>
2022-07-04 10:46:48 +0000

    a C function that interacts with python list

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * print_python_list_info - prints some basic info about Python lists
+ * @p: a pointer to a PyObject object
+ * 
+ * Return: void
+ */
+
+void print_python_list_info(PyObject *p)
+{
+		
+}

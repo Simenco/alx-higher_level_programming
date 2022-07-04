66b15638c1d1045adf8f87df6c28716f208b4bb0
Olumide, S.A <simiolumide@gmail.com>
2022-07-04 00:20:16 -0700

    replaces an element in a list without changing the original list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -11,6 +11,6 @@ def new_in_list(my_list, idx, element):
     if (idx < 0) or (idx >= ele):
         return my_list
     else:
-        my_new_list[] = my_list[]
+        my_new_list[] = my_list[:]
         my_new_list[idx] = element
         return my_new_list

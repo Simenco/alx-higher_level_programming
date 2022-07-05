c07e88480ceb3b17355f69a94e2d643cde324d68
Olumide, S.A <simiolumide@gmail.com>
2022-07-04 17:16:01 -0700

    returns the biggest number in a list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+this function finds the biggest integer of a list
+"""
+
+
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+
+    maxi = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > maxi:
+            maxi = my_list[i]
+
+    return (maxi)

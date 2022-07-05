d66bf11484f1529d57bc448c4ba223887af5aa98
Olumide, S.A <simiolumide@gmail.com>
2022-07-05 08:41:13 -0700

    check if linked list has a cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,24 +1,6 @@
 #include "lists.h"
 
 /**
- * list_len - function
- * @h: input list
- *
- * Return: number of elements in a list
- */
-unsigned int list_len(const listint_t *h)
-{
-	unsigned int n;
-
-	for (n = 0; h; n++)
-	{
-		h = h->next;
-	}
-	return (n);
-
-} 
-
-/**
  * check_cycle - this function checks if a singly linked list has a cycle in it
  * @list: a linked list to be checked
  *
@@ -27,44 +9,30 @@ unsigned int list_len(const listint_t *h)
 
 int check_cycle(listint_t *list)
 {
-	unsigned int n;
-
 	/* this approach uses two pointers to iterate through the list */
 
-	listint_t *iterator1 = malloc(sizeof(listint_t));
-	listint_t *iterator2 = malloc(sizeof(listint_t));
+	listint_t *iterator1, *iterator2;
 
-	/* check if the list is not empty */
-	if (list == NULL)
+	/* check if the list is not empty or just having 1 node */
+	if (list == NULL || list->next == NULL)
 		return (0);
 
-	n = list_len(list);
+	/* iterator1 moves 1 step while iterator 2 moves 2 steps each time*/
 
-	if (n == 1)
-		return (0);
+	iterator1 = list->next;
+	iterator2 = list->next->next;
 
-
-	iterator1 = list;
-	iterator2 = list;
-
-	/* let iterator1 move 1 step while iterator2 move 2 steps per iteration */
-	while (1)
+	while (iterator1 && iterator2 && iterator2->next)
 	{
-		iterator1 = iterator1->next;
-		iterator2 = (iterator2->next)->next;
-
 		if (iterator1 == iterator2)
 		{
 			return (1);
 		}
-		if ((iterator1 == NULL) || (iterator2 == NULL))
-		{
-			return (0);
-		}
-		else
-			continue;
+
+		iterator1 = iterator1->next;
+		iterator2 = iterator2->next->next;
 	}
-	free(iterator1);
-	free(iterator2);
+
+	return (0);
 
 }

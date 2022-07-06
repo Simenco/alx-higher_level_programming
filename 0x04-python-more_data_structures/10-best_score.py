df4541db7b2696535f2fa5bbed77f70b0665ac10
Olumide, S.A <simiolumide@gmail.com>
2022-07-06 16:52:11 -0700

    best score in class

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+a function that returns a key with the biggest integer value
+"""
+
+
+def best_score(a_dictionary):
+    if len(a_dictionary) == 0 or not isinstance(a_dictionary, dict):
+        return None
+
+    result = list(a_dictionary.keys())[0]
+    biggest = a_dictionary[result]
+    for key, value in a_dictionary.tems():
+        if value > biggest:
+            biggest = value
+            result = key
+    return

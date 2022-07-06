8b78152c1d9ee4d63229868298ba555f5e4c0db0
Olumide, S.A <simiolumide@gmail.com>
2022-07-06 16:20:42 -0700

    multiply values of a dictionary by 2

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""
+this function returns a dictionary with 
+all values multiplied by 2
+"""
+
+
+def multiply_by_2(a_dictionary):
+    return ({k, v*v for k, v in a_dictionary})

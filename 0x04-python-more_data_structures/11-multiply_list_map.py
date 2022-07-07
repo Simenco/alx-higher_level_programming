0581e15aa0f5f0774b09efd1043a67760455f1a2
Olumide, S.A <simiolumide@gmail.com>
2022-07-06 17:28:23 -0700

    multiply a list with a given number

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -7,4 +7,4 @@ a number without using any loops
 
 
 def multiply_list_map(my_list=[], number=0):
-    return ([list(map(lambda x:x*number, my_list))])
+    return list(map(lambda x:x*number, my_list))
